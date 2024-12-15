c9096652a2650b18c2c9ce7f01d3b83443e21ff3
Francisco Gemaque <gemaquejr@hotmail.com>
2024-12-15 15:51:29 -0300

    Adiciona som nas teclas do piano pelo teclado
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,10 +4,13 @@ let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`;
-    audio.play()
-}
+    audio.play();
+};
 
 pianoKeys.forEach((key) => {
-    console.log(key.dataset.key);
     key.addEventListener("click", () => playTune(key.dataset.key))
+});
+
+document.addEventListener("keydown", (e) => {
+    playTune(e.key);
 })
